005ef4055b8573c631f094bbed67ce59840ca53a
Johan Gardell <johan.gardell@distilnetworks.com>
2019-12-25 13:37:44 +0100

    Satisfy clippy

--- ensure_no_std/src/main.rs
+++ ensure_no_std/src/main.rs
@@ -29,10 +29,10 @@ use scoped_callback;
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    panic!()
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    panic!()
 }
